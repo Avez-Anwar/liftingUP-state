f4b9a230fad2e743675234a3622db9de3fd437d4
Avez-Anwar <34218292+Avez-Anwar@users.noreply.github.com>
2021-03-18 19:29:30 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Calculator from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Calculator />, rootElement);
